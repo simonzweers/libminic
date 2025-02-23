c1214398617bc65c4316be034226ac403de2edf5
Simon Zweers <simon.zweers@hva.nl>
2025-02-23 02:05:45 +0100

    Added basic project skeleton

---
+++ include/minic/llist.h
@@ -0,0 +1,6 @@
+#ifndef __LLIST_H__
+#define __LLIST_H__
+
+void hello_(void);
+
+#endif
---
+++ src/llist.c
@@ -0,0 +1,7 @@
+#include <minic/llist.h>
+
+#include <stdio.h>
+
+void hello_(void) {
+	printf("Hello World");
+}
